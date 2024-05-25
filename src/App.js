ce3876aeee3a0dc58ed47ff3a1ecf7c17509697e
arjit <64977636+arjitmehta@users.noreply.github.com>
2024-05-25 12:58:54 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,190 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import GenerateReport from "./GenerateReport";
 
 function App() {
+  const [initials, setInitials] = useState("");
+  const [country, setCountry] = useState("");
+  const [dob, setDob] = useState("");
+  const [age, setAge] = useState("");
+  const [ageUnit, setAgeUnit] = useState("Years");
+  const [sex, setSex] = useState("");
+  const [reactionOnset, setReactionOnset] = useState("");
+  const [reaction, setReaction] = useState("");
+  const [seriousness, setSeriousness] = useState("");
+
+  const calculateAge = (dob, reactionOnset, ageUnit, convertable) => {
+    let age_unit = ageUnit;
+    const dobDate = new Date(dob);
+    const onsetDate = new Date(reactionOnset);
+    const diffTime = Math.abs(onsetDate - dobDate);
+    console.log(diffTime);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    if (diffDays >= 366 && convertable) {
+      setAgeUnit("Years");
+      age_unit = "Years";
+    } else if (convertable) {
+      setAgeUnit("Months");
+      age_unit = "Months";
+    }
+
+    switch (age_unit) {
+      case "Months":
+        return Math.floor(diffDays / 30);
+      case "Years":
+        return Math.floor(diffDays / 365);
+      default:
+        return "";
+    }
+  };
+
+  const handleCalculateAge = () => {
+    if (dob && reactionOnset) {
+      setAge(calculateAge(dob, reactionOnset, ageUnit, true));
+    }
+  };
+
+  const handleInitialsChange = (e) => {
+    const value = e.target.value.toUpperCase();
+    if (/^[A-Z]*$/.test(value)) {
+      setInitials(value);
+    }
+  };
+
+  const handleReactionChange = (e) => {
+    const value = e.target.value;
+    if (/^[A-Za-z\s]*$/.test(value)) {
+      setReaction(value);
+    }
+  };
+
+  const handleSexChange = (event) => {
+    setSex(event.target.value);
+  };
+
+  const reportData = {
+    initials,
+    country,
+    dob,
+    age,
+    ageUnit,
+    sex,
+    reactionOnset,
+    reaction,
+    seriousness,
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <div className="form-group">
+          <label htmlFor="initials">Patient Initials:</label>
+          <input
+            type="text"
+            id="initials"
+            value={initials}
+            onChange={handleInitialsChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="country">Country:</label>
+          <select
+            id="country"
+            value={country}
+            onChange={(e) => setCountry(e.target.value)}
+          >
+            <option value="">Select Country</option>
+            <option value="GBR">GBR</option>
+            <option value="US">US</option>
+            <option value="IND">IND</option>
+            <option value="CAN">CAN</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="dob">Date of Birth:</label>
+          <input
+            type="date"
+            id="dob"
+            value={dob}
+            onChange={(e) => setDob(e.target.value)}
+            onBlur={handleCalculateAge}
+          />
+        </div>
+        <div className="form-group">
+          <label>Age:</label>
+          <input
+            type="number"
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+          />
+          <select
+            value={ageUnit}
+            onChange={(e) => {
+              setAgeUnit(e.target.value);
+              setAge(calculateAge(dob, reactionOnset, e.target.value, false));
+            }}
+          >
+            <option value="Months">Months</option>
+            <option value="Years">Years</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="sex">Sex:</label>
+          <div className="radio-button" style={{ display: "flex" }}>
+            <input
+              type="radio"
+              name="sex"
+              value="male"
+              checked={sex === "male"}
+              onChange={handleSexChange}
+            />
+            <label style={{ marginRight: 60, marginTop: 10 }}> Male </label>
+
+            <input
+              type="radio"
+              name="sex"
+              value="female"
+              checked={sex === "female"}
+              onChange={handleSexChange}
+            />
+            <label style={{ marginRight: 60, marginTop: 10 }}>Female </label>
+          </div>
+        </div>
+        <div className="form-group">
+          <label htmlFor="reactionOnset">Reaction Onset:</label>
+          <input
+            type="date"
+            id="reactionOnset"
+            value={reactionOnset}
+            onChange={(e) => setReactionOnset(e.target.value)}
+            onBlur={handleCalculateAge}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="reaction">Reaction:</label>
+          <input
+            type="text"
+            id="reaction"
+            value={reaction}
+            onChange={handleReactionChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="seriousness">Seriousness Criteria:</label>
+          <select
+            id="seriousness"
+            value={seriousness}
+            onChange={(e) => setSeriousness(e.target.value)}
+          >
+            <option value="">Select Criteria</option>
+            <option value="Death">Death</option>
+            <option value="Life threatening">Life threatening</option>
+            <option value="Hospitalization">Hospitalization</option>
+            <option value="Disability">Disability</option>
+          </select>
+        </div>
+        <GenerateReport reportData={reportData} />
+      </form>
     </div>
   );
 }
---
+++ src/GenerateReport.js
@@ -0,0 +1,174 @@
+import React from 'react';
+import htmlToPdfmake from 'html-to-pdfmake';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+const GenerateReport = ({ reportData }) => {
+  const generatePDF = () => {
+    const {
+      initials,
+      country,
+      dob,
+      age,
+      ageUnit,
+      sex,
+      reactionOnset,
+      reaction,
+      seriousness,
+    } = reportData;
+
+    const checked = `<svg width="10px" height="10px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+    <title>ic_fluent_checkbox_checked_24_regular</title>
+    <desc>Created with Sketch.</desc>
+    <g id="🔍-Product-Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="ic_fluent_checkbox_checked_24_regular" fill="#212121" fill-rule="nonzero">
+            <path d="M18.25,3 C19.7687831,3 21,4.23121694 21,5.75 L21,18.25 C21,19.7687831 19.7687831,21 18.25,21 L5.75,21 C4.23121694,21 3,19.7687831 3,18.25 L3,5.75 C3,4.23121694 4.23121694,3 5.75,3 L18.25,3 Z M18.25,4.5 L5.75,4.5 C5.05964406,4.5 4.5,5.05964406 4.5,5.75 L4.5,18.25 C4.5,18.9403559 5.05964406,19.5 5.75,19.5 L18.25,19.5 C18.9403559,19.5 19.5,18.9403559 19.5,18.25 L19.5,5.75 C19.5,5.05964406 18.9403559,4.5 18.25,4.5 Z M10,14.4393398 L16.4696699,7.96966991 C16.7625631,7.6767767 17.2374369,7.6767767 17.5303301,7.96966991 C17.7965966,8.23593648 17.8208027,8.65260016 17.6029482,8.94621165 L17.5303301,9.03033009 L10.5303301,16.0303301 C10.2640635,16.2965966 9.84739984,16.3208027 9.55378835,16.1029482 L9.46966991,16.0303301 L6.46966991,13.0303301 C6.1767767,12.7374369 6.1767767,12.2625631 6.46966991,11.9696699 C6.73593648,11.7034034 7.15260016,11.6791973 7.44621165,11.8970518 L7.53033009,11.9696699 L10,14.4393398 L16.4696699,7.96966991 L10,14.4393398 Z" id="🎨Color">
+</path>
+        </g>
+    </g>
+</svg>`
+    const unchecked = `<svg width="10px" height="10px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+    <title>ic_fluent_checkbox_unchecked_24_filled</title>
+    <desc>Created with Sketch.</desc>
+    <g id="🔍-Product-Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="ic_fluent_checkbox_unchecked_24_filled" fill="#212121" fill-rule="nonzero">
+            <path d="M6,3 L18,3 C19.6568542,3 21,4.34314575 21,6 L21,18 C21,19.6568542 19.6568542,21 18,21 L6,21 C4.34314575,21 3,19.6568542 3,18 L3,6 C3,4.34314575 4.34314575,3 6,3 Z M6,5 C5.44771525,5 5,5.44771525 5,6 L5,18 C5,18.5522847 5.44771525,19 6,19 L18,19 C18.5522847,19 19,18.5522847 19,18 L19,6 C19,5.44771525 18.5522847,5 18,5 L6,5 Z" id="🎨Color">
+</path>
+        </g>
+    </g>
+</svg>`
+
+    // Create HTML content for the report
+    const htmlContent = `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>CIOMS Form</title>
+        <style>
+          table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-bottom: 20px;
+          }
+          th,
+          td {
+            border: 1px solid black;
+            padding: 5px;
+            text-align: center;
+            font-size: 5px;
+          }
+          .des_rec {
+            border: 1px solid black;
+            vertical-align: top; 
+            text-align: left;
+            font-size: 5px;
+          }
+          .check_all {
+            border: 1px solid black;
+            vertical-align: top; 
+            text-align: left;
+            padding: 5px;
+            font-size: 5px;
+          }
+          .checkbox {
+            display: flex;
+            align-items: center;
+          }
+          .checkbox input {
+            margin-right: 5px;
+          }
+        </style>
+      </head>
+      <body>
+        <div style="text-align: right;">CIOMS FORM</div>
+        <table style="width:100%">
+          <tr>
+            <td   style="height:100px; text-align: center; font-size:10px; margin:50px; " rowspan="4" colspan="20"><text>SUSPECT ADVERSE REACTION REPORT</text></td>
+            <td style="height:33px" colspan="30"></td>
+          </tr>
+          <tr><td style="height:33px" colspan="30"></td></tr>
+          <tr style="height:30px">
+            <td colspan="14"></td>
+            <td colspan="1"></td>
+            <td colspan="1"></td>
+            <td colspan="1"></td>
+            <td colspan="1"></td>
+            <td colspan="1"></td>
+            <td colspan="1"></td>
+            <td colspan="1"></td>
+            <td colspan="1"></td>
+            <td colspan="1"></td>
+            <td colspan="1"></td>
+            <td colspan="1"></td>
+            <td colspan="1"></td>
+            <td colspan="1"></td>
+            <td colspan="1"></td>
+            <td colspan="1"></td>
+            <td colspan="1"></td>
+          </tr>
+        </table>
+    
+        <table>
+          <caption>I. REACTION INFORMATION</caption>
+          <tr>
+            <td style="font-size:10px" colspan="3" rowspan="2">1. PATIENT INITIALS <br/>(first, last)<br/> ${initials}</td>
+            <td style="font-size:10px" colspan="3" rowspan="2">1a. COUNTRY<br/>${country}</td>
+            <td style="font-size:10px" colspan="3">2. DATE OF BIRTH</td>
+            <td style="font-size:10px" colspan="3" rowspan="2">2a. AGE <br/>${ageUnit}<br/>${age}</td>
+            <td style="font-size:10px" colspan="3" rowspan="2">3. SEX<br/>${sex}</td>
+            <td style="font-size:10px" colspan="3">4-6. REACTION ONSET</td>
+            <td style="font-size:10px" class="check_all" colspan="6" rowspan="4">
+              <div style="font-size:10px" >8-12 CHECK ALL APPROPRIATE TO ADVERSE REACTION</div>
+              <div type="checkbox"/> ${seriousness.includes('Death') ? checked : unchecked} PATIENT DIED<br/>
+              <div type="checkbox"/> ${seriousness.includes('Hospitalization') ? checked : unchecked} INVOLVED OR PROLONGED INPATIENT HOSPITALISATION<br/>
+              <div type="checkbox"/> ${seriousness.includes('Disability') ? checked : unchecked} INVOLVED PERSISTENCE OR SIGNIFICANT DISABILITY OR INCAPACITY<br/>
+              <div type="checkbox"/> ${seriousness.includes('Life threatening') ? checked : unchecked} LIFE THREATENING
+              
+           </td>
+          </tr>
+          <tr>
+            <td style="font-size:10px"Date<br/>/date here/</td>
+            <td style="font-size:10px">Month<br/>/month here/</td>
+            <td style="font-size:10px">Year<br/>/year here/</td>
+            <td style="font-size:10px">Date<br/>/date here/</td>
+            <td style="font-size:10px">Month<br/>/month here/</td>
+            <td style="font-size:10px">Year<br/>/year here/</td>
+          </tr>
+          <tr>
+            <td style="font-size:10px" class="des_rec" colspan="18" rowspan="3">
+              7 + 13. DESCRIBE REACTION(S) (including relevant tests/lab data)<br/>${reaction}
+            </td>
+          </tr>
+        </table>
+      </body>
+    </html>
+    
+    `
+
+    // Convert HTML to pdfMake format
+    const pdfContent = htmlToPdfmake(htmlContent,{
+        tableAutoSize:true,
+      });
+
+    const docDefinition = {
+      content: pdfContent,
+    };
+
+    // Generate PDF
+    pdfMake.createPdf(docDefinition).download('report.pdf');
+  };
+
+  return (
+    <button type="button" onClick={generatePDF}>
+      Generate Report
+    </button>
+  );
+};
+
+export default GenerateReport;
